2604d205f64e548d2a82699637f1c942f6b0fc12
robiiinos <dyrda.robin@outlook.fr>
2021-01-01 22:52:02 +0100

    fix: Default route configuration

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,10 @@
 import { NowRequest, NowResponse } from "@vercel/node";
-import { version } from "../package.json";
+import { name, description, version } from "../package.json";
 
 export default (req: NowRequest, res: NowResponse): void => {
   res.json({
-    name: "serverless-vercel-api",
-    description: "Serverless API Template, for Vercel hosting.",
+    name,
+    description,
     version,
   });
 };
