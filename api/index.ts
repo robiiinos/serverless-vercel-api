618dc67f72dfbd05dfed859a3b2ccceedbac6efb
robiiinos <dyrda.robin@outlook.fr>
2023-01-02 11:37:41 +0100

    feat: Response payload

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,18 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { name, description, version } from "../package.json";
 
-export default (_: VercelRequest, res: VercelResponse): void => {
-  res.json({
-    name,
-    description,
-    version,
+export default (req: VercelRequest, res: VercelResponse): VercelResponse => {
+  if (req.method?.toUpperCase() === "OPTIONS") {
+    return res.status(204).end();
+  }
+
+  return res.status(200).json({
+    success: true,
+    error: null,
+    data: {
+      name,
+      description,
+      version,
+    },
   });
 };
--- api/time.ts
+++ api/time.ts
@@ -1,7 +1,15 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
-export default (_: VercelRequest, res: VercelResponse): void => {
-  const time = new Date().getTime();
+export default (req: VercelRequest, res: VercelResponse): VercelResponse => {
+  if (req.method?.toUpperCase() === "OPTIONS") {
+    return res.status(204).end();
+  }
 
-  res.json({ time });
+  return res.status(200).json({
+    success: true,
+    error: null,
+    data: {
+      time: new Date().getTime(),
+    },
+  });
 };
